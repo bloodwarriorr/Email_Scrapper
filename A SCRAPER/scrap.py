0414461767a7613123345932a7badffa2588731b
bloodwarriorr <kenar529@gmail.com>
2022-07-14 00:09:48 +0300

    Add threading to the main program,result handling and comments.

--- A SCRAPER/scrap.py
+++ A SCRAPER/scrap.py
@@ -1,17 +1,22 @@
 from tkinter import *
-from tkinter import filedialog
 from tkinter import messagebox
 from bs4 import BeautifulSoup
 import requests.exceptions
 import urllib.parse
 from collections import deque
 import re
+import threading
+import os
 
 
-def email_scrapper():
-    sent_url = user_url.get()
-    if 'https://' not in sent_url:
-        sent_url = 'https://' + user_url.get()
+def email_scrapper(main_window, user_url):
+    """
+    This functions recive url address,scrapping through 20 hyperlinks inside the root url and extracting them into a list
+    :param main_window: tkinter
+    :param user_url: string
+    :return: None
+    """
+    sent_url = "https://" + user_url.get()
     urls = deque([sent_url])
     scraped_urls = set()
     emails = set()
@@ -49,6 +54,26 @@ def email_scrapper():
         print('[-] Closing!')
 
     export_scan_to_file(list(emails))
+    handle_results(main_window)
+
+
+def handle_results(main_window):
+    """
+    This function handle the result after url scrapping, decide to watch the list of scrapped emails or exit
+    :param main_window: tkinter
+    :return: None
+    """
+    response = messagebox.askokcancel("Finished Successfully!", "Open the text file to watch results!")
+    label = Label(main_window, text=response)
+    label.pack()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    if response == 1:
+        os.startfile(dir_path + "/Emails.txt")
+        main_window.quit()
+    else:
+        exit_response = messagebox.showwarning("Exit", "Bye Bye!")
+        Label(main_window, text=exit_response).pack()
+        main_window.quit()
 
 
 def export_scan_to_file(emails):
@@ -61,16 +86,20 @@ def export_scan_to_file(emails):
             fh.write(f"{mail + 1}){emails[mail]}\n")
 
 
-main_window = Tk()
-main_window.title("Email Scrapper")
-main_window.geometry('500x300')
-main_window.config(bg='azure')
-user_url = StringVar(main_window)
-Label(main_window, text='The Email Scrapper', bg='azure', fg='black', font=('Times', 20, 'bold')).place(x=100, y=10)
-Label(main_window, text='Enter Url With https://', bg='azure2', anchor="e", justify=LEFT).place(x=225, y=90)
-Entry(main_window, textvariable=user_url, width=35, font=('calibre', 10, 'normal')).place(x=150, y=140)
-Button(main_window, text="Start", bg='ivory3', font=('calibre', 13), command=email_scrapper).place(x=225, y=180)
-main_window.mainloop()
+def main():
+    # init the tkinter window, add the scrapping functionality to the start button
+    main_window = Tk()
+    main_window.title("Email Scrapper")
+    main_window.geometry('500x300')
+    main_window.config(bg='azure')
+    user_url = StringVar(main_window)
+    Label(main_window, text='The Email Scrapper', bg='azure', fg='black', font=('Times', 20, 'bold')).place(x=100, y=10)
+    Label(main_window, text='Enter Url', bg='azure2', anchor="e", justify=LEFT).place(x=225, y=90)
+    Entry(main_window, textvariable=user_url, width=35, font=('calibre', 10, 'normal')).place(x=150, y=140)
+    Button(main_window, text="Start", bg='ivory3', font=('calibre', 13),
+           command=threading.Thread(target=email_scrapper, args=(main_window, user_url)).start).place(x=225, y=180)
+    main_window.mainloop()
 
 
-
+if __name__ == '__main__':
+    main()
